a51b3c177608959efa0f5da4fa72def4d5453848
BalvirPurewal <balvirp1@gmail.com>
2024-05-29 14:33:05 -0500

    :sparkles: US-2: Add relationshipts between user and task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -24,5 +25,10 @@ class Task extends Model
             'expiration_date' => 'datetime',
             'is_complete' => 'boolean'
         ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
